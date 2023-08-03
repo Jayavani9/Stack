b872b21a0e995042361fc979648f35574d28f1f4
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-03 15:23:19 -0700

    Update BasicCalcII.java

--- BasicCalcII.java
+++ BasicCalcII.java
@@ -16,3 +16,38 @@ Output: 1
 Example 3:
 Input: s = " 3+5 / 2 "
 Output: 5
+
+
+class Solution {
+    public int calculate(String s) {
+        if(s == null || s.isEmpty()) return 0;
+        int num = 0;
+        char sign = '+';
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0 ; i < s.length();i++)
+        {
+            char c = s.charAt(i);
+            if(Character.isDigit(c))
+            {
+                num = num * 10 + (c-'0');
+
+            }
+            if(!Character.isDigit(c) && c!= ' ' || i == s.length()-1)
+            {
+                if(sign == '+') st.push(num);
+                else if(sign == '-') st.push(-num);
+                else if(sign == '*') st.push(st.pop()*num);
+                else if(sign == '/') st.push(st.pop()/num);
+                 sign = c;
+                 num = 0;
+            }
+        }
+
+        int res = 0;
+        while(!st.isEmpty())
+        {
+        res = res + st.pop();
+        }
+        return res;
+    }
+}
