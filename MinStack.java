10c445c9e8f7e53e2fcfe062995adccf324a05aa
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-11-05 17:05:54 -0800

    Update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -1,3 +1,6 @@
+
+1. Using a min variable 
+    
 class MinStack {
     public Stack<Integer> st;
     public Stack<Integer> minStack;
@@ -41,4 +44,57 @@ class MinStack {
  obj.pop();
  int param_3 = obj.top();
  int param_4 = obj.getMin();
+
+
+2. Without using a min variable
+
+
+class MinStack {
+
+    public Stack<Integer> st = new Stack<>();
+    public Stack<Integer> minSt = new Stack<>();
+
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+        
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if (minSt.isEmpty() || val <= minSt.peek()) {
+        minSt.push(val);
+        }
+        else 
+        {
+            minSt.push(minSt.peek());
+        }
+     
+    }
+    
+    public void pop() {
+        st.pop();
+        minSt.pop();
+  
+    }
+    
+    public int top() {
+        return st.peek();
+        
+    }
+    
+    public int getMin() {
+        return minSt.peek();
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
  
