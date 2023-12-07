dcc95b8643cebace4cbae12b8c594d4082219edd
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-12-06 16:44:39 -0800

    Update bckspstrcomp.java

--- bckspstrcomp.java
+++ bckspstrcomp.java
@@ -20,3 +20,33 @@ Input: s = "a#c", t = "b"
 Output: false
 Explanation: s becomes "c" while t becomes "b".
 
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+       Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '#') {
+                if (!st1.isEmpty()) {
+                    st1.pop();
+                }
+            } else {
+                st1.push(c);
+            }
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            char ch = t.charAt(j);
+            if (ch == '#') {
+                if(!st2.isEmpty())
+                {
+                st2.pop();
+                }
+            } else {
+                st2.push(ch);
+            }
+        }
+        return st1.equals(st2);
+    }
+}
